66b098795aabe9559f44feb9e72cff386f011b31
mercwrite <markandrew1805@gmail.com>
2024-10-19 00:58:00 -0500

    Fixed loading error

--- src/app/clientlayout.js
+++ src/app/clientlayout.js
@@ -3,31 +3,11 @@ import NavBar from "./components/navbar";
 import { SearchProvider } from "./context/searchcontext";
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Suspense, useEffect } from "react";
-import nProgress from 'nprogress';
-import 'nprogress/nprogress.css'; 
-import { usePathname, useSearchParams } from 'next/navigation';
+import { Suspense } from "react"; 
+
 
 function ClientLayoutComponent({ children }){
-
-    const pathname = usePathname();
-    const searchParams = useSearchParams();
   
-    useEffect(() => {
-      nProgress.start(); 
-  
-      // Simulate loading delay
-      const timeout = setTimeout(() => {
-        nProgress.done(); 
-      }, 100); // Adjust delay as needed
-  
-      return () => {
-        clearTimeout(timeout);
-        nProgress.done(); 
-      };
-    }, [pathname, searchParams]);
-
-
     return (
         <SearchProvider>
             <NavBar/>
