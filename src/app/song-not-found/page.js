a2c84a3ab422ec8ae477358f845b8e74e766dc81
mercwrite <markandrew1805@gmail.com>
2024-10-27 15:12:26 -0500

    Fixed escape error

--- src/app/song-not-found/page.js
+++ src/app/song-not-found/page.js
@@ -5,7 +5,7 @@ export default function Page () {
     <Suspense>
         <div className="flex flex-col items-center justify-center h-screen pt-12 bg-graybg p-12">
             <h1 className="text-4xl font-bold text-white text-center">
-                Oops! It seems like this song is unavailable or doesn't exist.
+                Oops! It seems like this song is unavailable or doesn&apos;t exist.
             </h1>
         </div>
     </Suspense>
