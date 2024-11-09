0c672cfaea2b2a4d33a587e0761563b461d8612b
mercwrite <markandrew1805@gmail.com>
2024-11-08 20:12:41 -0600

    Handle undefined songid

--- src/app/track/[id]/page.js
+++ src/app/track/[id]/page.js
@@ -1,14 +1,21 @@
 'use client';
 import { useParams } from 'next/navigation';
 import SongDisplay from '@/app/components/songdisplay';
+import { useRouter } from "next/navigation";
 
-export default function TrackPage(){
+export default function TrackPage() {
     const params = useParams();
-    const { id } = params;
+    const router = useRouter();
+    const { id } = params || {};
+
+    // Check if id exists to avoid rendering with undefined songId
+    if (!id) {
+        router.push("/song-not-found");
+    }
 
     return (
         <div>
-            <SongDisplay key={SongDisplay} songId={id}/>
+            <SongDisplay songId={id} />
         </div>
     );
 }
